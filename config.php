ebb86fa5021a72217d2b8881d4995d0cf20e54e7
jpastorm <jospastorm@upt.pe>
2020-06-11 22:01:04 -0500

    archivos restantes

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$db = [
+    'host' => '192.185.7.14',
+    'username' => 'sapon_trevaadmin',
+    'password' => 'Sistemas.123!',
+    'db' => 'saponte_db_treva' //Cambiar al nombre de tu base de datos
+];
+
+?>
