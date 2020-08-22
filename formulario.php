f81741e42c3a1473141eb22d7bfbad0b5fd62cb7
sigfredo99 <saponte99@gmail.com>
2020-08-21 22:26:02 -0500

    Home diseño

--- formulario.php
+++ formulario.php
@@ -32,14 +32,14 @@ if (!isset($_SESSION['id_usuario'])) {
   <div class="wrapper ">
     <div class="sidebar" data-color="white" data-active-color="danger">
       <div class="logo">
-        <a href="#" class="simple-text logo-mini">
-          <!-- <div class="logo-image-small">
+      <a href="#" class="simple-text logo-mini">
+          <div class="logo-image-small">
             <img src="./assets/img/logo-small.png">
-          </div> -->
+          </div>
           <!-- <p>CT</p> -->
         </a>
         <a href="#" class="simple-text logo-normal">
-          Your Logo
+          TREVA
           <!-- <div class="logo-image-big">
             <img src="../assets/img/logo-big.png">
           </div> -->
@@ -59,12 +59,7 @@ if (!isset($_SESSION['id_usuario'])) {
               <p>Formularios</p>
             </a>
           </li>
-          <li>
-            <a href="javascript:;">
-              <i class="nc-icon nc-chart-bar-32"></i>
-              <p>Estadisticas</p>
-            </a>
-          </li>
+     
         </ul>
       </div>
     </div>
@@ -81,7 +76,7 @@ if (!isset($_SESSION['id_usuario'])) {
                 <span class="navbar-toggler-bar bar3"></span>
               </button>
             </div>
-            <a class="navbar-brand" href="javascript:;">Treva</a>
+            <a class="navbar-brand" href="javascript:;">Formularios</a>
           </div>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-bar navbar-kebab"></span>
@@ -89,16 +84,7 @@ if (!isset($_SESSION['id_usuario'])) {
             <span class="navbar-toggler-bar navbar-kebab"></span>
           </button>
           <div class="collapse navbar-collapse justify-content-end" id="navigation">
-            <form>
-              <div class="input-group no-border">
-                <input type="text" value="" class="form-control" placeholder="Buscar...">
-                <div class="input-group-append">
-                  <div class="input-group-text">
-                    <i class="nc-icon nc-zoom-split"></i>
-                  </div>
-                </div>
-              </div>
-            </form>
+      
             <ul class="navbar-nav">
               <li class="nav-item btn-rotate dropdown">
                 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -108,9 +94,9 @@ if (!isset($_SESSION['id_usuario'])) {
                   </p>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="#">Salir</a>
-                  <a class="dropdown-item" href="#">Perfil de Usuario</a>
-                  <a class="dropdown-item" href="#">also mas aqui</a>
+                  <a class="dropdown-item" href=".">Salir</a>
+             
+             
                 </div>
               </li>
             </ul>
